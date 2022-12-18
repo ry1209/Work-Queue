e409f31e119372c81dcfc35595b72cfe00c44a3e
Richard Yeh <lichang971209@gmail.com>
2022-12-18 20:11:44 +1100

    fix typo

--- web_automation/go_frontend.py
+++ web_automation/go_frontend.py
@@ -3,7 +3,7 @@ from typing import Optional
 from Config import config
 from RPA.Browser.Selenium import Selenium
 
-class GoActoinsElements:
+class GoActionsElements:
     PLAYER_INFO_BUTTON = lambda x: fr'xpath://html/body/table[2]/tbody/tr[{str(x+1)}]/td[2]/a'
     PLAYER_NAME = lambda x: fr'xpath://html/body/table[2]/tbody/tr[{str(x+1)}]/td[2]/a'
     NUMBER_OF_WINS = r'xpath://html/body/table[1]/tbody/tr[1]/td'
@@ -27,7 +27,7 @@ class GoBase:
         self.driver.maximize_browser_window()
     
     def get_player_name(self, player_rank : int) -> str:
-        return self.driver.find_element(GoActoinsElements.PLAYER_NAME(player_rank)).text
+        return self.driver.find_element(GoActionsElements.PLAYER_NAME(player_rank)).text
     
     def tear_down(self) -> None:
         self.driver.close_browser()
@@ -36,7 +36,7 @@ class GoBase:
 class GoActions(GoBase):
 
     def get_player_win_rate(self, player_rank : int) -> int:
-        self.driver.click_element_when_visible(GoActoinsElements.PLAYER_INFO_BUTTON(player_rank))
-        wins = int(self.driver.find_element(GoActoinsElements.NUMBER_OF_WINS).text)
-        total_games = int(self.driver.find_element(GoActoinsElements.TOTAL_GAMES_PLAYED).text)
+        self.driver.click_element_when_visible(GoActionsElements.PLAYER_INFO_BUTTON(player_rank))
+        wins = int(self.driver.find_element(GoActionsElements.NUMBER_OF_WINS).text)
+        total_games = int(self.driver.find_element(GoActionsElements.TOTAL_GAMES_PLAYED).text)
         return round(wins/total_games,2)
